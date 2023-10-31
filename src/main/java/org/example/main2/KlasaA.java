e54375b166bc2e17e0b924b1411a15322788a12f
MichałSuchanek <MSuchanek97@gmail.com>
2023-10-31 13:14:06 +0100

    cos

--- src/main/java/org/example/main2/KlasaA.java
+++ src/main/java/org/example/main2/KlasaA.java
@@ -1,3 +1,15 @@
 package org.example.main2;
 
-public class KlasaA {}
+public class KlasaA {
+    public static void main(String[] args){
+
+    int a = 5;
+    int b = 0
+
+    if (10>5){
+      System.out.println(a);
+      b = 10
+    }
+    }
+
+}
